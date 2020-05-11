db2aaa82592276a6064f7283e8c42405fc336717
requiemnao <65193474+requiemnao@users.noreply.github.com>
2020-05-11 19:59:57 -0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+int main() 
+{
+int i=0;
+printf("+");
+for(i=0;i<23;i++)
+    printf("-");
+    printf("+\n");
+for (i=0;i<3;i++){
+        printf("| o | X | o | X | o | X |");
+        printf("\n");
+        printf("| X | o | X | o | X | o |");
+        printf("\n");
+}
+printf("+");
+    for(i=0;i<23;i++)
+    printf("-");
+    printf("+");
+return(0);
+}
