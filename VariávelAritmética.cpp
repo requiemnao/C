1a7f860ec0b53d80ae11c160080fd8480ec98c8e
requiemnao <65193474+requiemnao@users.noreply.github.com>
2020-05-12 16:10:42 -0300

    Rename Untitled1.cpp to VariávelAritmética.cpp

--- VariávelAritmética.cpp
+++ VariávelAritmética.cpp
@@ -4,4 +4,4 @@ int main()
     printf("Dear Procrastinator,\nYou still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!",25-23,60*24*2,60*60*24*2);
     return 0;
 }
-// utilizando a variável %d para fazer calcúlos aritméticos
+// utilizando a variÃ¡vel %d para fazer calcÃºlos aritmÃ©ticos
