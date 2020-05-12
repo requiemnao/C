0d798625c6a1bce132a647b1d20ce31e9805e22e
requiemnao <65193474+requiemnao@users.noreply.github.com>
2020-05-12 16:06:19 -0300

    Add files via upload

---
+++ Untitled1.cpp
@@ -0,0 +1,7 @@
+#include <stdio.h>
+int main()
+{
+    printf("Dear Procrastinator,\nYou still have to wait for %d days (%d minutes or %d seconds) before you can procrastinate!",25-23,60*24*2,60*60*24*2);
+    return 0;
+}
+// utilizando a variável %d para fazer calcúlos aritméticos
